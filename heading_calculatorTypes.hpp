9fde43d5a657ff574ca11ecdd1f1f9673ee842ac
Stefan Haase <stefan.haase@dfki.de>
2013-08-27 13:47:33 +0200

    initial commit by rock-create-orogen

---
+++ heading_calculatorTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef heading_calculator_TYPES_HPP
+#define heading_calculator_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace heading_calculator {
+}
+
+#endif
+
